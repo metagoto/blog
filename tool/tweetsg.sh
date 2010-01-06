d2bdd53ddcd645eec0b023a9920888b10e16c3ba
metagoto <runpac314@gmail.com>
2010-01-06 03:03:36 +0100

    cleaning

--- tool/tweetsg.sh
+++ tool/tweetsg.sh
@@ -9,11 +9,8 @@ oldsum=""
 notify="curl -s $4"
 sump=$5
 
-echo "start $rss" >> /Users/runpac/cron/log.txt
-
 if [ -f $rss ] 
 then
-  echo $($sump $rss) >> /Users/runpac/cron/log.txt
   oldsum=$($sump $rss)
 fi
 
@@ -27,18 +24,12 @@ fi
 
 sum=$($sump $rss)
 
-echo "2nd sum $sum" >> /Users/runpac/cron/log.txt
-
 if [ "$sum" != "$oldsum" ]
 then
   if ! $parser
   then
-    echo "!= " >> /Users/runpac/cron/log.txt
     exit 1
   else
-	echo "notify " >> /Users/runpac/cron/log.txt
 	$notify
   fi
-else 
-  echo "== " >> /Users/runpac/cron/log.txt
 fi
